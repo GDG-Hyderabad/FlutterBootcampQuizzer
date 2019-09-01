bb398b4c0320edddac91b5cdcc94393b8b209d46
Hemasaicharan <hscharan2010@gmail.com>
2019-09-01 12:05:10 +0530

    WM

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-git c
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
